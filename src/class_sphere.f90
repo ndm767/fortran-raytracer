b89a4ec618d6d7ed6de685b81ef231242eebe063
ndm767 <5031736+ndm767@users.noreply.github.com>
2022-04-19 15:22:23 -0700

    added simple sphere tracing

---
+++ src/class_sphere.f90
@@ -0,0 +1,46 @@
+module class_sphere
+    use :: class_vec3
+    use :: class_ray
+
+    implicit none 
+
+    private 
+    public :: sphere, sphere_create, sphere_hit
+
+    type sphere
+        type(vec3) :: c
+        real :: r
+    end type sphere
+
+    contains
+
+    function sphere_create(center, radius) result (s)
+        type(vec3), intent(in) :: center
+        real, intent(in) :: radius
+        type(sphere) :: s
+        s%c = center
+        s%r = radius
+    end function sphere_create
+
+    function sphere_hit(curr_ray, curr_sphere) result (did_hit)
+        type(ray), intent(in) :: curr_ray
+        type(sphere), intent(in) :: curr_sphere
+        logical :: did_hit
+        type(vec3) :: oc
+        real :: a, b, c, disc
+
+        oc = curr_ray%orig - curr_sphere%c
+        a = vec3_dot(curr_ray%dir, curr_ray%dir)
+        b = vec3_dot(oc * 2.0, curr_ray%dir)
+        c = vec3_dot(oc, oc) - curr_sphere%r**2
+
+        disc = b*b - 4.0 * a * c
+
+        if (disc > 0) then
+            did_hit = .true.
+        else
+            did_hit = .false.
+        end if
+
+    end function sphere_hit
+end module class_sphere
--- src/main.f90
+++ src/main.f90
@@ -1,5 +1,7 @@
 program Raytracer
     use :: class_vec3
+    use :: class_ray
+    use :: class_sphere
 
     implicit none
 
@@ -8,23 +10,15 @@ program Raytracer
     integer :: x, y ! loop variables
     real :: r, g, b ! color variables
     integer :: ir, ig, ib ! integer colors
-    type(vec3) :: v1, v2, v3
-    v1 = vec3_create(1.0, 2.0, 3.0)
-    v2 = vec3_create(3.0, 2.0, 1.0)
-    v3 = v2 - v1
-    call vec3_print(v1)
-    call vec3_print(v2)
-    call vec3_print(v3)
-    print *, "V1 len = ", vec3_length(v1)
-    v2 = v2 * 2.0
-    v1 = v1 / 2.0
-    call vec3_print(v1)
-    call vec3_print(v2)
-    call vec3_print(vec3_normalize(v2))
+    type(ray) :: pixel_ray ! ray for camera
+    type(vec3) :: ray_color ! ray output color
+    type(sphere) :: main_sphere
 
     ! variable definitions
     width = 400
     height = 400
+
+    main_sphere = sphere_create(vec3_create(0.0, 0.0, 2.0), 1.0)
 
     ! open the output file
     open(1, file = "image.ppm", status = "new")
@@ -32,16 +26,42 @@ program Raytracer
 
     do x = 0, width-1
         do y = 0, height-1
-            r = real(x) / (real(width) - 1.0)
-            g = real(y) / (real(height) - 1.0)
-            b = 0.25
+            r = real(x) / ((real(width) - 1.0)/2.0) - 1.0
+            g = real(y) / ((real(height) - 1.0)/2.0) - 1.0
+            b = 0.0
 
-            ir = int(255.99 * r)
-            ig = int(255.99 * g)
-            ib = int(255.99 * b)
+            pixel_ray = ray_create(vec3_create(r, g, b), vec3_create(0.0, 0.0, 1.0))
+            ray_color = get_ray_color(pixel_ray, main_sphere)
+
+
+            ir = int(255.99 * ray_color%v(1))
+            ig = int(255.99 * ray_color%v(2))
+            ib = int(255.99 * ray_color%v(3))
 
             write(1, '(I3,A,I3,A,I3,/)', advance='no') ir, " ", ig, " ", ib
 
         end do 
     end do 
+
+contains
+    function get_ray_color(r, s) result (c)
+        use :: class_ray
+        use :: class_vec3
+        use :: class_sphere
+
+        implicit none
+        
+        type(ray), intent(in) :: r
+        type(sphere), intent(in) :: s
+        type(vec3) :: c
+
+        if (sphere_hit(r, s) .eqv. .true.) then
+            c = vec3_create(1.0, 0.0, 0.0)
+        else
+            c = vec3_create(0.0, 0.0, 0.0)
+        end if
+
+    end function get_ray_color
+
+
 end program Raytracer
